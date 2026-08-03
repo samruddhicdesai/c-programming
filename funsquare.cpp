2176783f261ee16732f350bb306c48ab2f898e69
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-03 10:44:55 +0530

    Add files via upload

---
+++ funsquare.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void square()
+{
+int  num;
+cout<<"Enter the number = ";
+cin>>num;
+cout<<"Square is : "<<num*num;
+}
+int main()
+{
+square();
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
